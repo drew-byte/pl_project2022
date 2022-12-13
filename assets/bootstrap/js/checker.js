ac4db4596e3831f24ef39adf3aaf2ad044708907
drew <82567656+drew-byte@users.noreply.github.com>
2022-12-13 18:34:24 +0800

    finished will update soon

--- assets/bootstrap/js/checker.js
+++ assets/bootstrap/js/checker.js
@@ -6,103 +6,108 @@ function getChecked() {
     let finalArray = [];
     const choices = ["String", "int", "char", "double"];
     const x = 0;
-    var method = "";
 
-    array.forEach(element => {
+    if (array.length === 5 || array.length === 4) {
 
-        
+        array.forEach(element => {
+            if (element.includes(choices[0]) || element.includes(choices[1]) || element.includes(choices[2]) || element.includes(choices[3])) {
+                finalArray[x] = "<data_type>";
 
-        if (element.includes(choices[0]) || element.includes(choices[1]) || element.includes(choices[2]) || element.includes(choices[3])) {
-            finalArray[x] = "<data_type>";
+            }
+            else if (element.includes("=")) {
+                finalArray[x + 2] = "<assignment_operator>";
 
-        }
-        else if (element.includes("=")) {
-            finalArray[x + 2] = "<assignment_operator>";
+            }
+            else if (element.includes(";")) {
+                if (val == 0 && element.includes("\"") || element.includes(";")
+                    || element.includes("'")) {
+                    finalArray[x + 3] = "<value>";
+                    finalArray[x + 4] = "<delimiter>";
 
-        }
-        else if (element.includes(";")) {
-            if (val == 0 && element.includes("\"") || element.includes(";")
-                || element.includes("'")) {
+                } else if (element.includes(";")) {
+                    finalArray[x + 4] = "<delimiter>";
+
+                }
+
+            }
+            else if (val == 0 && element.includes("\"") || element.includes("'")) {
+                val++;
                 finalArray[x + 3] = "<value>";
-                finalArray[x + 4] = "<delimiter>";
 
-            } else if (element.includes(";")) {
-                finalArray[x + 4] = "<delimiter>";
-
+            } else {
+                finalArray[x + 1] = "<identifier>";
             }
 
-        }
-        else if (val == 0 && element.includes("\"") || element.includes("'")) {
-            val++;
-            finalArray[x + 3] = "<value>";
+            const getSyntax = finalArray.join(" ");
+            document.getElementById("lexiBG").style.backgroundColor = "green";
+            if (getSyntax === "<data_type> <identifier> <assignment_operator> <value> <delimiter>" || getSyntax === "<data_type> <identifier> <delimiter>") {
+                const semanticDataType = array[0];
+                const semanticVal = array[3];
+                document.getElementById("synBG").style.backgroundColor = "green";
 
-        } else {
-            finalArray[x + 1] = "<identifier>";
-        }
-
-        method = finalArray;
-
-
-    });
-
-    const getSyntax = finalArray.join(" ");
-
-    if(getSyntax==="<data_type> <identifier> <assignment_operator> <value> <delimiter>" || getSyntax==="<data_type> <identifier> <delimiter>")
-    {
-        const semanticDataType = array[0];
-        const semanticVal = array[3];
-        
-            try {
-                if (
-                    semanticDataType === "int" &&
-                    !semanticVal.includes(".") &&
-                    !semanticVal.includes("'") &&
-                    !semanticVal.includes('"')
-                ) {
-                    document.getElementById('cardOutput').textContent = finalArray;
+                try {
+                    if (
+                        semanticDataType === "int" &&
+                        !semanticVal.includes(".") &&
+                        !semanticVal.includes("'") &&
+                        !semanticVal.includes('"')
+                    ) {
+                        document.getElementById('cardOutput').textContent = finalArray;
+                        document.getElementById("semBG").style.backgroundColor = "green";
+                    }
+                    else if (
+                        (semanticDataType === "double" || semanticDataType === "float") &&
+                        semanticVal.includes(".") &&
+                        !semanticVal.includes("'") &&
+                        !semanticVal.includes('"')
+                    ) {
+                        document.getElementById('cardOutput').textContent = finalArray;
+                        document.getElementById("semBG").style.backgroundColor = "green";
+                    }
+                    else if (semanticDataType === "String" && semanticVal.includes('"')) {
+                        document.getElementById('cardOutput').textContent = finalArray;
+                        document.getElementById("semBG").style.backgroundColor = "green";
+                    }
+                    else if (semanticDataType === "boolean" && (semanticVal.includes("true") || semanticVal.includes("false")) &&
+                        !value.includes(".") &&
+                        !value.includes("'") &&
+                        !value.includes('"')
+                    ) {
+                        document.getElementById('cardOutput').textContent = finalArray;
+                        document.getElementById("semBG").style.backgroundColor = "green";
+                    }
+                    else if (semanticDataType === "char" && semanticVal.length === 3 && semanticVal.includes("'")) {
+                        document.getElementById('cardOutput').textContent = finalArray;
+                        document.getElementById("semBG").style.backgroundColor = "green";
+                    }
+                    else {
+                        document.getElementById('cardOutput').textContent = "Semantically Error!";
+                    }
+                    document.getElementById("semBG").style.backgroundColor = "red";
                 }
-                else if (
-                    (semanticDataType === "double" || semanticDataType === "float") &&
-                    semanticVal.includes(".") &&
-                    !semanticVal.includes("'") &&
-                    !semanticVal.includes('"')
-                ) {
-                    document.getElementById('cardOutput').textContent = finalArray;
-                }
-                else if (semanticDataType === "String" && semanticVal.includes('"')) {
-                    document.getElementById('cardOutput').textContent = finalArray;
-                }
-                else if (semanticDataType === "boolean" && (semanticVal.includes("true") || semanticVal.includes("false")) &&
-                    !value.includes(".") &&
-                    !value.includes("'") &&
-                    !value.includes('"')
-                ) {
-                    document.getElementById('cardOutput').textContent = finalArray;
-                }
-                else if (semanticDataType === "char" && semanticVal.length === 3 && semanticVal.includes("'")) {
-                    document.getElementById('cardOutput').textContent = finalArray;
-                }
-                else
-                {
+                catch (e) {
                     document.getElementById('cardOutput').textContent = "Semantically Error!";
+                    document.getElementById("semBG").style.backgroundColor = "red";
                 }
+
+
             }
-            catch (e) {
-                document.getElementById('cardOutput').textContent = "Semantically Error!";
+            else {
+                document.getElementById('cardOutput').textContent = "Syntax Error!";
+                document.getElementById("synBG").style.backgroundColor = "red";
+
             }
-       
+
+        });
     }
-    else
-    {
-        document.getElementById('cardOutput').textContent = "Syntax Error!";
-        
+    else {
+        document.getElementById('cardOutput').textContent = "Lexical Error!";
+        document.getElementById("lexiBG").style.backgroundColor = "red";
     }
-
-
-
-
-    
 }
+
+
+
 
 
 
